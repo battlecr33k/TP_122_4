c13e5e957ab6a3aac3698da303c899165d71646b
19553 <19553@1k122s4.dune.edu>
2021-11-18 15:03:35 +0200

    init

---
+++ src/tplab/TPlab.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tplab;
+
+/**
+ *
+ * @author 19553
+ */
+public class TPlab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
