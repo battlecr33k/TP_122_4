8ad6fb319bc0e472980426526194862cb52262bc
19553 <19553@1k122s4.dune.edu>
2021-11-18 15:12:02 +0200

    2 file

---
+++ src/tplab/Comp4.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tplab;
+
+/**
+ *
+ * @author 19553
+ */
+public class Comp4 {
+    
+}
