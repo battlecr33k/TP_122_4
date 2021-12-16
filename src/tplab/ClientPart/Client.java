9fe7fefdb5e210034b15b22ef1326abfee35f0ae
19553 <19553@1k122s4.dune.edu>
2021-12-16 14:42:58 +0200

    ClientPart

---
+++ src/tplab/ClientPart/Client.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tplab.ClientPart;
+
+/**
+ *
+ * @author 19553(Ziulikov)
+ */
+public class Client {
+    //Клиентская часть приложения
+}
